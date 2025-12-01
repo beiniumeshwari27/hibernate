cb44d4629d36d96202736867fb92ad80b1614b67
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-01 22:59:59 +0530

    java programs

--- Employee.java
+++ Employee.java
@@ -1,26 +1,14 @@
 package com.codegnan.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-
 public class Employee {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
 	private String name;
 	private String department;
 	private double salary;
 	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
 	}
-	public Employee(String name, String department, double salary) {
+	public Employee(int id, String name, String department, double salary) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.department = department;
 		this.salary = salary;
@@ -51,17 +39,7 @@ public class Employee {
 	}
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", department=" + department + ", salary=" + salary + "]";
+		return id + " | " + name + " | " + department + " | " + salary;
 	}
-	public Employee(int id, String name, String department, double salary) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.department = department;
-		this.salary = salary;
-	}
-	
-	
-	
-
 }
+ 
