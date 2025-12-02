1e03c0b9b37c18d8b6e6443ed22248275dab32c9
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-02 10:58:23 +0530

    java programs

--- Employee.java
+++ Employee.java
@@ -1,45 +1,34 @@
 package com.codegnan.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String department;
 	private double salary;
-	public Employee() {
-	}
-	public Employee(int id, String name, String department, double salary) {
+	public Employee(String name, String department, double salary) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.department = department;
 		this.salary = salary;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDepartment() {
-		return department;
-	}
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	@Override
-	public String toString() {
-		return id + " | " + name + " | " + department + " | " + salary;
-	}
+	
+
 }
- 
