d184e766c4b2e33461730192316ff1602be62228
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-08 22:15:30 +0530

    java programs

--- Employee.java
+++ Employee.java
@@ -1,34 +1,40 @@
 package com.codegnan.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@AllArgsConstructor
 @Entity
 public class Employee {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	private String name;
-	private String department;
-	private double salary;
-	public Employee(String name, String department, double salary) {
-		super();
-		this.name = name;
-		this.department = department;
-		this.salary = salary;
-	}
-	
+private int empId;
+private String empName;
+private double  empSalary;
+@OneToOne(cascade=CascadeType.ALL)
+private Address address;
+
+public Employee(String empName,double empSalary) {
+	super();
+	this.empName = empName;
+	this.empSalary = empSalary;
+}
+
+@Override
+public String toString() {
+	return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + "]";
+}
 
 }
