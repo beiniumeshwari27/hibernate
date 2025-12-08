db865e57e43c98ca7c8bd80ffa44cc82a4ca38c3
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-08 22:17:50 +0530

    java programs

--- App.java
+++ App.java
@@ -1,56 +1,71 @@
 package com.codegnan;
 
-import java.util.List;
+//import org.hibernate.Transaction;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.codegnan.entity.Address;
 import com.codegnan.entity.Employee;
-import com.codegnan.exception.InvalidEmployeeIdException;
-import com.codegnan.service.EmployeeService;
-import com.codegnan.service.EmployeeServiceImpl;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-       EmployeeService service=new EmployeeServiceImpl();
-       //add employee
-      /* Employee e1=new Employee("Teju","sales",30000);
-       //int id=service.addEmployee(e1);
-       System.out.println("Inserted Employee id :"+id);*/
-     //search the employee with id
-       /* try {
- 		Employee emp=service.getEmployeeById(1);
- 		System.out.println("Employee details : "+emp);
- 	} catch (InvalidEmployeeIdException e) {
- 		// TODO Auto-generated catch block
- 		e.printStackTrace();
- 	}*/
-        //List of all Employees
-        /*System.out.println("All Employees");
-        List<Employee>employees=service.getAllEmployees();
-        employees.forEach(System.out::println);*/
-        
-        /*Employee emp=new Employee(1,"kumar","training ",57300);
+        StandardServiceRegistry registry=null;
+        SessionFactory sessionFactory=null;
         try {
- 		service.updateEmployee(emp);
- 		System.out.println("Employee updated successfully");
- 	} catch (InvalidEmployeeIdException e) {
- 		// TODO Auto-generated catch block
- 		e.printStackTrace();
- 	}*/
-        
-        
-        //delete the employee 4
-        try {
- 		service.deleteEmployee(4);
- 		System.out.println("Employee deleted successfully");
- 	} catch (InvalidEmployeeIdException e) {
- 		
- 		e.printStackTrace();
- 	}
+        	//step-1 load configuration and build session factory
+        	registry=new StandardServiceRegistryBuilder()
+        			.configure("hibernate.cfg.xml").build();
+        	Metadata metadata=new MetadataSources(registry)
+        			.getMetadataBuilder().build();
+        	sessionFactory=metadata.getSessionFactoryBuilder().build();
+        	try (Session session=sessionFactory.openSession()){
+        		/*Transaction tx=session.beginTransaction();
+        		Address address=new  Address("Hyderabad","telangana","500001");
+        		Employee employee=new Employee("ravikumar",65000);
+        		address.setEmployee(employee);
+        		employee.setAddress(address);
+        		session.save(employee);
+        		tx.commit();
+        		System.out.println("record inserted successfully");
+        		System.out.println(employee);*/
+        		System.out.println("Retriving Employee and address");
+        		Employee emp=session.get(Employee.class,1);
+        		if(emp!=null) {
+        			System.out.println("Employee Found");
+        			System.out.println("Employee Id:"+emp.getEmpId());
+        			System.out.println("Employee Name:"+emp.getEmpName());
+        			System.out.println("Employee Salary:"+emp.getEmpSalary());
+        			Address addr=emp.getAddress();
+        			if(addr!=null) {
+        				System.out.println("Address Details");
+        				System.out.println("City:"+addr.getCity());
+        				System.out.println("State:"+addr.getState());
+        				System.out.println("Pin Code:"+addr.getPincode());
+        			}else {
+        				System.out.println("No Address Found For This Employee");
+        			}	
+        		}else {
+        			System.out.println("Employee Not Found");
+        		}
+        	}catch(Exception e) {
+        		e.printStackTrace();
+        	}
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }finally {
+        	if(sessionFactory!=null) {
+        		sessionFactory.close();
+        	}if(registry!=null) {
+        		StandardServiceRegistryBuilder.destroy(registry);
+        	}
+        }
     }
-       
 }
