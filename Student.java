411cc037642ce665141a02978a5ad47a8bbf735d
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-15 10:59:49 +0530

    java programs

--- Student.java
+++ Student.java
@@ -1,28 +1,43 @@
-package com.codegnan.beans;
+package com.codegnan.model;
 
-import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@AllArgsConstructor
 @Entity
-
 public class Student {
 	@Id
-private String sid;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+private int sid;
 private String sname;
-private String saddr;
-@ManyToMany(cascade=CascadeType.ALL)
-private Set<Course>courses;
+private String city;
+private int marks;
+@ManyToOne
+@JoinColumn(name="branch_id")
+private Branch branch;
+public Student(String sname, String city, int marks, Branch branch) {
+	super();
+	this.sname = sname;
+	this.city = city;
+	this.marks = marks;
+	this.branch = branch;
+}
+@Override
+public String toString() {
+	return "Student [sid=" + sid + ", sname=" + sname + ", city=" + city + ","
+			+ " marks=" + marks +
+			", branch=" + branch.getBname()
+			+ "]";
+}
 }
