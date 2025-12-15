48a63a0b90caaa3676fee1401e5ad5cd05fe3e01
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-12-15 10:58:19 +0530

    java programs

--- Employee.java
+++ Employee.java
@@ -1,7 +1,13 @@
-package com.codegnan.model;
+package com.codegnan.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,24 +15,26 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Employee {
-	private int empId;
-	private String empName;
-	private double empSalary;
-	public Employee(String empName, double empSalary) {
-		super();
-		this.empName = empName;
-		this.empSalary = empSalary;
-	}
-	@Override
-	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + "]";
-	}
-	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+private int empId;
+private String empName;
+private double  empSalary;
+@OneToOne(cascade=CascadeType.ALL)
+private Address address;
 
-	
-	}
+public Employee(String empName,double empSalary) {
+	super();
+	this.empName = empName;
+	this.empSalary = empSalary;
+}
 
+@Override
+public String toString() {
+	return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + "]";
+}
 
+}
